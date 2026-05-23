5d777708ed346699ef3d4fbb6880d65c3c7469e8
Oliwier Zyskowski <oliwierzyskowski04@gmail.com>
2026-05-24 00:17:19 +0200

    init commit

---
+++ src/main/java/org/example/votingsytsem/VotingSytsemApplication.java
@@ -0,0 +1,13 @@
+package org.example.votingsytsem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSytsemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingSytsemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/votingsytsem/VotingSytsemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.votingsytsem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSytsemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
